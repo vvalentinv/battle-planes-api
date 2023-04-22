2f31ccefd2541e6009fb5b30acb9440283761a19
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-04-22 17:40:26 -0400

    added values validation in helpers and used it in the two endpoints

--- service/user.py
+++ service/user.py
@@ -1,14 +1,16 @@
 from dao.user import UserDao
 from exception.forbidden import Forbidden
-from utilities.helper import validate_password
+from utilities.helper import validate_password, validate_username, validate_email, validate_password_value
 
 
 class UserService:
     def __init__(self):
         self.user_dao = UserDao()
 
-    def add_user(self,  user):
-        # TO DO validate values
+    def add_user(self, user):
+        if validate_username(user.get_username()) and \
+                validate_email(user.get_email()) and validate_password_value(user.get_password()):
+            pass
         if self.user_dao.check_for_username(user.get_username()):
             raise Forbidden("This username is already in use! Please try again.")
         if self.user_dao.check_for_email(user.get_email()):
@@ -16,7 +18,8 @@ class UserService:
         return self.user_dao.add_user(user)
 
     def update_email(self, username, password, email):
-        # TO DO validate email, password values
+        if validate_email(email):
+            pass
         if not validate_password(password, self.user_dao.get_user_by_username(username).get_password()):
             raise Forbidden("Invalid password for this account!")
         return self.user_dao.update_email(username, email)
--- utilities/helper.py
+++ utilities/helper.py
@@ -1,8 +1,75 @@
 import bcrypt
+import re
+from exception.invalid_parameter import InvalidParameter
 
 
 def hash_registering_password(passwd):
     return bcrypt.hashpw(passwd, bcrypt.gensalt())
 
-def validate_password(passwd, hash):
-    return bcrypt.checkpw(passwd.encode(), hash.encode())
+
+def validate_password(passwd, pw_hash):
+    return bcrypt.checkpw(passwd.encode(), pw_hash.encode())
+
+
+def validate_email(string):
+    reg_email = r"[^@]+@[^@]+\.[^@]+"
+    if not string:
+        raise InvalidParameter("Email cannot be blank")
+    if not re.match(reg_email, string):
+        raise InvalidParameter("Accepted email address format is: username@domain.domain_type")
+    return True
+
+
+def validate_password_value(string):
+    if not string:
+        raise InvalidParameter("password cannot be blank")
+    elif not 20 >= len(string) >= 8:
+        raise InvalidParameter("Accepted password length is between 8 and 20 characters inclusive")
+
+    if string:
+        alphabetical_characters = "abcdefghijklmnopqrstuvwxyz"
+        special_characters = "!@#$%^&*"
+        numeric_characters = "0123456789"
+
+        lower_alpha_count = 0
+        upper_alpha_count = 0
+        special_character_count = 0
+        numeric_character_count = 0
+
+        for char in string:
+            if char in alphabetical_characters:
+                lower_alpha_count += 1
+            elif char in alphabetical_characters.upper():
+                upper_alpha_count += 1
+            elif char in special_characters:
+                special_character_count += 1
+            elif char in numeric_characters:
+                numeric_character_count += 1
+            else:
+                raise InvalidParameter(
+                    "Password must contain only alphanumeric and special characters only from this set (!@#$%^&*)")
+        if lower_alpha_count < 1:
+            raise InvalidParameter("Password must have at least 1 lowercase character")
+        if upper_alpha_count < 1:
+            raise InvalidParameter("Password must have at least 1 uppercase character")
+        if special_character_count < 1:
+            raise InvalidParameter("Password must have at least 1 special (!@#$%^&*) character")
+        if numeric_character_count < 1:
+            raise InvalidParameter("Password must have at least 1 numeric character")
+
+    return True
+
+
+def validate_username(string):
+    reg_invalid_character = r"[^a-zA-Z0-9]"
+
+    if not string:
+        raise InvalidParameter("Usernames cannot be blank")
+    if re.findall(reg_invalid_character, string):
+        raise InvalidParameter("Usernames must have only alphanumeric characters")
+    elif len(string) > 30:
+        raise InvalidParameter("Usernames are limited to 30 alphanumeric characters ")
+    elif len(string) < 4:
+        raise InvalidParameter("Usernames must have at least 4 alphanumeric characters")
+
+    return True
