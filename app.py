fcefbcc3271b1cb1ea65d92c962b26086db7c835
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-04-22 09:14:44 -0400

    registered user controller blueprint

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 
+from controller.user import uc
+
 if __name__ == '__main__':
     app = Flask(__name__)
+
+    app.register_blueprint(uc)
 
     app.run(port=5000, debug=True)
