12524572999d63963caff01f58c02b861c91bed6
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-04-22 04:35:52 -0400

    added empty dao layer functions for add_user business logic

---
+++ dao/user.py
@@ -0,0 +1,10 @@
+class UserDao:
+    def add_user(self, user):
+        pass
+
+    def get_user_by_username(self, username):
+        pass
+
+    def get_user_by_email(self, email):
+        pass
+    
