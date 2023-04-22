39f2c012958c8387adaff1fb8ea5b6a0ec586065
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-04-22 09:32:26 -0400

    added function to add user and check for exising username and email

--- dao/user.py
+++ dao/user.py
@@ -1,10 +1,43 @@
+from model.user import User
+from utilities.helper import hash_registering_password
+from dotenv import load_dotenv
+import os
+import psycopg2
+
+
+load_dotenv()
+os.getenv("ACCESS_KEY")
+
+
 class UserDao:
     def add_user(self, user):
-        pass
+        h_pass = hash_registering_password(user.get_password().encode('utf-8'))
+        with psycopg2.connect(database=os.getenv("db_name"), user=os.getenv("db_user"),
+                              password=os.getenv("db_password"), host=os.getenv("db_host"),
+                              port=os.getenv("db_port")) as conn:
+            with conn.cursor() as cur:
+                cur.execute("INSERT INTO users (username, pass, email) "
+                            "VALUES (%s, %s, %s) RETURNING *", (user.get_username(),  h_pass.decode(), user.get_email()))
+                inserted_user = cur.fetchone()
+                if inserted_user:
+                    return "User successfully added!"
+                return None
 
-    def get_user_by_username(self, username):
-        pass
+    def check_for_username(self, username):
+        with psycopg2.connect(database=os.getenv("db_name"), user=os.getenv("db_user"),
+                              password=os.getenv("db_password"), host=os.getenv("db_host"),
+                              port=os.getenv("db_port")) as conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT  (SELECT username FROM users WHERE username=%s) = %s", (username, username))
+                check = cur.fetchone()
+                return check[0]
 
-    def get_user_by_email(self, email):
-        pass
+    def check_for_email(self, email):
+        with psycopg2.connect(database=os.getenv("db_name"), user=os.getenv("db_user"),
+                              password=os.getenv("db_password"), host=os.getenv("db_host"),
+                              port=os.getenv("db_port")) as conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT  (SELECT username FROM users WHERE email=%s) = %s", (email, email))
+                check = cur.fetchone()
+                return check[0]
     
