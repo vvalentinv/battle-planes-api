482a5162702da107e501fbc64d5f3c9dd02ed87c
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-04-22 09:22:58 -0400

    renamed util package to utilities and added helper file

--- utilities/DDL.sql
+++ utilities/DDL.sql
@@ -1,13 +1,14 @@
 DROP TABLE IF EXISTS users;
 
---CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 --CREATE EXTENSION IF NOT EXISTS intarray;
 
 CREATE TABLE users(
 	id SERIAL PRIMARY KEY,
-	username VARCHAR(30) NOT NULL UNIQUE,
-	pass BYTEA NOT NULL,
-	email VARCHAR(30) NOT NULL UNIQUE,
-	token_value BYTEA
+	username TEXT NOT NULL UNIQUE,
+	pass TEXT NOT NULL,
+	email TEXT NOT NULL UNIQUE
 );
 
+SELECT  (SELECT username FROM users WHERE username='jd80@a.ca') = 'jd80@a.ca';
+select * from users u ;
