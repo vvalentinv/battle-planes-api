a894834bd15720fa1e81a7b0d23e895d352a0211
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-04-22 04:33:10 -0400

    added business logic for add_user in service layer

--- service/user.py
+++ service/user.py
@@ -1,5 +1,15 @@
+from dao.user import UserDao
+from exception.forbidden import Forbidden
+
+
 class UserService:
+    def __init__(self):
+        self.user_dao = UserDao()
 
     def add_user(self, user):
-        pass
-
+        if self.user_dao.get_user_by_username(user.username):
+            raise Forbidden("This username is already in use! Please try again.")
+        if self.user_dao.get_user_by_email(user.email):
+            raise Forbidden("This email is already in use! Please sign into your existing account.")
+        added_user_obj = self.user_dao.add_user(user)
+        return added_user_obj.to_dict()
