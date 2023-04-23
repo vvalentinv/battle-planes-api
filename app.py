85747222f70658c8ff22ac3a3e095a162fb52592
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-04-23 07:38:57 -0400

    registered plane bluprint

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
 from controller.user import uc
+from controller.plane import pc
 
 if __name__ == '__main__':
     app = Flask(__name__)
 
     app.register_blueprint(uc)
+    app.register_blueprint(pc)
 
     app.run(port=5000, debug=True)
