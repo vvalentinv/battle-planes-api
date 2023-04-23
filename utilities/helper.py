9dcfbf1bf8d96302aa963db5ec5c7e9becb8484e
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-04-23 16:04:47 -0400

    fixed bug in build all planes function

--- utilities/helper.py
+++ utilities/helper.py
@@ -76,67 +76,64 @@ def validate_username(string):
 
 
 def build_plane(cockpit, sky_size, flight_direction):
-    plane = ()
-    plane += cockpit
+    plane = [cockpit]
     if flight_direction == 'N':
         # Add wings
         for i in range(-2, 3, 1):
-            plane += cockpit + (i + sky_size)
+            plane.append(cockpit + (i + sky_size))
         # Add body
-        plane += cockpit + (2 * sky_size)
+        plane.append(cockpit + (2 * sky_size))
         # Add tail
         for i in range(-1, 2, 1):
-            plane += cockpit + (i + (3 * sky_size))
+            plane.append(cockpit + (i + (3 * sky_size)))
     if flight_direction == 'W':
         # Add wings
         for i in range(-2, 3, 1):
-            plane += cockpit + (i * sky_size) + 1
+            plane.append(cockpit + (i * sky_size) + 1)
         # Add body
-        plane += cockpit + 2
+        plane.append(cockpit + 2)
         # Add tail
         for i in range(-1, 2, 1):
-            plane += cockpit + (i * sky_size) + 3
+            plane.append(cockpit + (i * sky_size) + 3)
     if flight_direction == 'E':
         # Add wings
         for i in range(-2, 3, 1):
-            plane += cockpit + (i * sky_size) - 1
+            plane.append(cockpit + (i * sky_size) - 1)
         # Add body
-        plane += cockpit - 2
+        plane.append(cockpit - 2)
         # Add tail
         for i in range(-1, 2, 1):
-            plane += cockpit + (i * sky_size) - 3
+            plane.append(cockpit + (i * sky_size) - 3)
     if flight_direction == 'S':
         # Add wings
         for i in range(-2, 3, 1):
-            plane += cockpit - (i + sky_size)
+            plane.append(cockpit - (i + sky_size))
         # Add body
-        plane += cockpit - (2 * sky_size)
+        plane.append(cockpit - (2 * sky_size))
         # Add tail
         for i in range(-1, 2, 1):
-            plane += cockpit - (i + (3 * sky_size))
+            plane.append(cockpit - (i + (3 * sky_size)))
+    plane = tuple(plane)
     return plane
 
 
 def build_plane_data(plane, sky_size, flight_direction):
-    plane_data = ()
-    plane_data += plane
-    plane_data += sky_size
-    plane_data += flight_direction
-    return plane_data
+    plane_data = [plane, sky_size, flight_direction]
+    return tuple(plane_data)
 
 
 def build_all_planes_for_sky_size(sky_size, plane_length, wings_size):
     planes_data = set()
     for c in range(0, sky_size * sky_size):
-        while wings_size - 1 < c % sky_size < sky_size - wings_size and c < sky_size * (sky_size - (plane_length - 1)):
+        if wings_size - 1 < c % sky_size < sky_size - wings_size and c < sky_size * (sky_size - (plane_length - 1)):
             planes_data.add(build_plane_data(build_plane(c, sky_size, 'N'), sky_size, 'N'))
-        while wings_size - 1 < c % sky_size < sky_size - wings_size and c > sky_size * (plane_length - 1):
+        if wings_size - 1 < c % sky_size < sky_size - wings_size and c > sky_size * (plane_length - 1):
             planes_data.add(build_plane_data(build_plane(c, sky_size, 'S'), sky_size, 'S'))
-        while 0 <= c % sky_size < sky_size - (plane_length - 1) and \
-                wings_size - 1 < c < sky_size * (sky_size - wings_size):
+        if 0 <= c % sky_size < sky_size - (plane_length - 1) and \
+                wings_size * sky_size <= c < sky_size * (sky_size - wings_size):
             planes_data.add(build_plane_data(build_plane(c, sky_size, 'W'), sky_size, 'W'))
-        while plane_length - 1 <= c % sky_size < sky_size and \
-                wings_size * sky_size < c < sky_size * (sky_size - wings_size):
+        if plane_length - 1 <= c % sky_size < sky_size and \
+                wings_size * sky_size + plane_length - 1 <= c < sky_size * (sky_size - wings_size):
             planes_data.add(build_plane_data(build_plane(c, sky_size, 'E'), sky_size, 'E'))
     return planes_data
 
