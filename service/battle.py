29b79c5b8914e2a954a9de0a45f370c967b7c448
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-04-24 09:08:10 -0400

    added validation function for new battle record

--- service/battle.py
+++ service/battle.py
@@ -1,20 +1,39 @@
 from dao.battle import BattleDao
 from dao.plane import PlaneDao
+from dao.user import UserDao
+from utilities.helper import validate_int, validate_flight_direction, validate_username
+from exception.invalid_parameter import InvalidParameter
 
 
 class BattleService:
     def __init__(self):
         self.battle_dao = BattleDao()
         self.plane_dao = PlaneDao()
+        self.user_dao = UserDao()
 
     def add_plane_to_battle_defense_by_username(self, battle_id, cockpit, flight_direction, sky_size):
-        # TO DO validate values
-        # TO DO check if logged-in user is in battle
+        # TO DO check if logged-in user is one of battle users
         # TO DO bring username as param
-        plane_id = self.plane_dao.get_plane_id(cockpit, flight_direction, sky_size)
-        username = None
-        if plane_id is not None:
-            # get current defense array of plane Ids for username and add plane_id to it
-        planes_array = None
-        return self.battle_dao.add_plane_to_battle_defense_by_username(battle_id, planes_array, username)
+        if validate_int(battle_id) and validate_int(cockpit) and validate_flight_direction(flight_direction) \
+                and validate_int(sky_size) and 9 < sky_size < 16:
+            plane_id = self.plane_dao.get_plane_id(cockpit, flight_direction, sky_size)
+            username = None
+            plane_ids = None
+            if plane_id is not None:
+                position = self.battle_dao.get_defense_position(battle_id, username)
+                plane_ids = list(self.battle_dao.get_defense_by_id_username_and_position(battle_id, username, position))
+                plane_ids.append(plane_id)
+            return self.battle_dao.add_plane_to_battle_defense_by_username(battle_id, plane_ids, username)
+        else:
+            raise InvalidParameter("Battlefield size is between 10 and 15 inclusive.")
+
+    def add_new_battle(self, user_id, opponent):
+        opponent_user = None
+        if validate_username(opponent):
+            opponent_user = self.user_dao.get_user_by_username(opponent)
+        elif self.battle_dao.check_if_user_is_battle_ready:
+            return f"{opponent} has already engaged in battle!"
+        return self.battle_dao.add_battle(user_id, opponent_user.get_user_id())
+
+
 
