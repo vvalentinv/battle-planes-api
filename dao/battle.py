e805ab970d367a766d5a60ce9c4529bd41708fc9
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-04-24 09:09:05 -0400

    added placeholders in dao layer

--- dao/battle.py
+++ dao/battle.py
@@ -7,5 +7,17 @@ load_dotenv()
 
 
 class BattleDao:
+    def __init__(self):
+        self.check_if_user_is_battle_ready = None
+
     def add_plane_to_battle_defense_by_username(self, battle_id, planes_array, username):
+        pass
+
+    def get_defense_position(self, battle_id, username):
+        pass
+
+    def get_defense_by_id_username_and_position(self, battle_id, username, position):
+        pass
+
+    def add_battle(self, user_id, opponent_id):
         pass
