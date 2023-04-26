df69e2bdd07b795fa649bfa42b0342090a95439b
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-04-26 03:56:51 -0400

    updated DDL added default challenger insert

--- utilities/DDL.sql
+++ utilities/DDL.sql
@@ -18,7 +18,7 @@ CREATE TABLE flight_directions(
 
 CREATE TABLE planes(
 	id SERIAL PRIMARY KEY,
-	cockpit int NOT NULL,
+	cockpit INT NOT NULL,
 	flight_direction_id INT NOT NULL,
 	body INT[] NOT NULL,
 	sky_size INT NOT NULL,
@@ -28,14 +28,24 @@ CREATE TABLE planes(
 
 CREATE TABLE battles(
 	id SERIAL PRIMARY KEY,
-	cockpit int NOT NULL,
-	flight_direction_id INT NOT NULL,
-	body INT[] NOT NULL,
-	sky_size INT NOT NULL,
-	CONSTRAINT fk_flight_direction_id
-  		FOREIGN KEY (flight_direction_id) REFERENCES "flight_directions" (id)
+	challenger_id INT DEFAULT 0,
+	challenged_id INT NOT NULL,
+	challenger_defense INT[],
+	challenged_defense INT[] NOT NULL,
+	sky_size INT DEFAULT 10,
+	challenger_attacks INT[],
+	challenged_attacks INT[],
+	consec_rnd_attack_count INT DEFAULT 0,
+	concluded INT DEFAULT 0,
+	defense_size INT DEFAULT 3,
+	battle_turn TIMESTAMP,
+	CONSTRAINT fk_challenger_id
+  		FOREIGN KEY (challenger_id) REFERENCES "users" (id),
+  	CONSTRAINT fk_challenged_id
+  		FOREIGN KEY (challenged_id) REFERENCES "users" (id)
 );
 
+INSERT INTO users (id, username, pass, email) VALUES (0, 'default-challenger', '', '');
 INSERT INTO flight_directions VALUES (1, 'North'), (2, 'East'), (3, 'South'), (4, 'West');
 
 select * from flight_directions f ;
