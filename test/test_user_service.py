c35886a19a8d29e3ceda97f8eba55f049530ab5c
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-05-05 05:04:07 -0400

    added first two unit tests

--- test/test_user_service.py
+++ test/test_user_service.py
@@ -1,5 +1,26 @@
 import pytest
 
+from controller.user import user_service
+from exception.forbidden import Forbidden
 
-def check_for_username_valid(mocker):
-    pass
+
+def test_check_for_username_existing(mocker):
+    # Arrange
+    def mock_check_for_username(self, username):
+        return True
+
+    mocker.patch('dao.user.UserDao.check_for_username', mock_check_for_username)
+    # Act and  # Assert
+    with pytest.raises(Forbidden):
+        user_service.add_user('jcad1', 'Password123!!', 'a@a.ca')
+
+
+def test_check_for_email_existing(mocker):
+    # Arrange
+    def mock_check_for_email(self, email):
+        return True
+
+    mocker.patch('dao.user.UserDao.check_for_email', mock_check_for_email)
+    # Act and  # Assert
+    with pytest.raises(Forbidden):
+        user_service.add_user('jcad1', 'Password123!!', 'a@a.ca')
