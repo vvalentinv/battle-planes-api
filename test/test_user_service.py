2516a7e81f405bee9a93a93903f6a643aa69c72a
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-05-06 05:06:06 -0400

    finished user service unit tests with 100% coverage

--- test/test_user_service.py
+++ test/test_user_service.py
@@ -45,7 +45,7 @@ def test_add_user_valid_data(mocker):
     assert actual == expected
 
 
-def test_get_user_by_username_invalid_current_password(mocker):
+def test_update_user_invalid_current_password(mocker):
     # Arrange
     def mock_get_user_by_username(self, username):
         return User(None, 'jcad1', '$2b$12$kcvn4uWQAKdu.ZJ1Mv4KV./XBKlIrjTiNkcARUxBZMdCuUC.JixoG', 'a@a.ca')
@@ -54,6 +54,64 @@ def test_get_user_by_username_invalid_current_password(mocker):
     # Act and  # Assert
     with pytest.raises(Forbidden):
         user_service.update_user('jcad1', 'Password123!!', 'Password123!!2', 'a@a.ca')
+
+
+def test_update_user_valid_current_password_invalid_email(mocker):
+    # Arrange
+    def mock_get_user_by_username(self, username):
+        return User(None, 'jcad1', '$2b$12$kcvn4uWQAKdu.ZJ1Mv4KV./XBKlIrjTiNkcARUxBZMdCuUC.JixoG', 'a@a.ca')
+
+    def mock_check_for_email(self, email):
+        return True
+
+    mocker.patch('dao.user.UserDao.get_user_by_username', mock_get_user_by_username)
+    mocker.patch('dao.user.UserDao.check_for_email', mock_check_for_email)
+    # Act and  # Assert
+    with pytest.raises(Forbidden):
+        user_service.update_user('jcad1', 'Password123!!2', 'Password123!!', 'a@a.ca')
+
+
+def test_update_user_valid_current_password_valid_email(mocker):
+    # Arrange
+    def mock_get_user_by_username(self, username):
+        return User(None, 'jcad1', '$2b$12$kcvn4uWQAKdu.ZJ1Mv4KV./XBKlIrjTiNkcARUxBZMdCuUC.JixoG', 'a@a.ca')
+
+    def mock_check_for_email(self, email):
+        return False
+
+    mocker.patch('dao.user.UserDao.get_user_by_username', mock_get_user_by_username)
+    mocker.patch('dao.user.UserDao.check_for_email', mock_check_for_email)
+    # Actual
+    actual = user_service.update_user('jcad1', 'Password123!!2', 'Password123!!', 'a1234@a.ca')
+    expected = 'Email successfully updated!'
+    # Assert
+    assert actual == expected
+
+
+def test_update_user_invalid_current_password_no_email(mocker):
+    # Arrange
+    def mock_get_user_by_username(self, username):
+        return User(None, 'jcad1', '$2b$12$kcvn4uWQAKdu.ZJ1Mv4KV./XBKlIrjTiNkcARUxBZMdCuUC.JixoG', 'a@a.ca')
+
+    mocker.patch('dao.user.UserDao.get_user_by_username', mock_get_user_by_username)
+
+    # Act and  # Assert
+    with pytest.raises(Forbidden):
+        user_service.update_user('jcad1', 'Password123!!', 'Password123!!', None)
+
+
+def test_update_user_valid_current_password_no_email(mocker):
+    # Arrange
+    def mock_get_user_by_username(self, username):
+        return User(None, 'jcad1', '$2b$12$kcvn4uWQAKdu.ZJ1Mv4KV./XBKlIrjTiNkcARUxBZMdCuUC.JixoG', 'a@a.ca')
+
+    mocker.patch('dao.user.UserDao.get_user_by_username', mock_get_user_by_username)
+
+    # Actual
+    actual = user_service.update_user('jcad1', 'Password123!!2', 'Password123!!', None)
+    expected = 'Password successfully updated!'
+    # Assert
+    assert actual == expected
 
 
 # input_validation_helper tests
@@ -154,5 +212,3 @@ def test_invalid_password_format_missing_numeric():
     # Act and  # Assert
     with pytest.raises(InvalidParameter):
         user_service.add_user('jcad1', 'Pabcderf!', 'a@a.ca')
-
-
