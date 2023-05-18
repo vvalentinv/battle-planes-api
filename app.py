353f0a520e0b6d93bb6808d0e116ebfffc97a53e
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-05-18 07:53:45 -0400

    added Coors to app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 from controller.user import uc
 from controller.plane import pc
@@ -6,9 +7,12 @@ from controller.battle import bc
 
 if __name__ == '__main__':
     app = Flask(__name__)
+    app.config['CORS_HEADERS'] = 'Content-Type'
 
     app.register_blueprint(uc)
     app.register_blueprint(pc)
     app.register_blueprint(bc)
+
+    CORS(app, origins=['http://127.0.0.1:5137'])
 
     app.run(port=5000, debug=True)
