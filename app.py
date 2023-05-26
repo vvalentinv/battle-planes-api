900bc5a796e10a8fe1a95f6c4529a06a25992825
Valentin Vlad <valentin_gabriel.vlad@yahoo.com>
2023-05-25 20:19:19 -0400

    added missing plane controller for db buildup

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from datetime import datetime
 from datetime import timedelta
 from datetime import timezone
+
+# from controller.plane import pc
 from controller.user import uc
 from controller.battle import bc
 from controller.auth import ac
@@ -26,6 +28,8 @@ if __name__ == '__main__':
     app.config["JWT_ACCESS_CSRF_HEADER_NAME"] = "X-CSRF-TOKEN-ACCESS"
     app.config["JWT_REFRESH_CSRF_HEADER_NAME"] = "X-CSRF-TOKEN-REFRESH"
     jwt = JWTManager(app)
+    # required when adding all planes for a sky_size to db
+    # app.register_blueprint(pc)
     app.register_blueprint(bc)
     app.register_blueprint(uc)
     app.register_blueprint(ac)
