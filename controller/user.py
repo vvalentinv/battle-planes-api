26f9706a89d0920cca1039872695c095ef996eed
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-06-19 06:00:55 -0400

    added email to GET /users and preflight to user update route

--- controller/user.py
+++ controller/user.py
@@ -26,21 +26,31 @@ def add_user():
         return {"message": str(e)}, 403
 
 
-@uc.route('/users', methods=['PUT'])
+@uc.route('/users', methods=['PUT', 'OPTIONS'])
 @jwt_required()
 def update_user():
-    r_body = request.get_json()
-    user_id = get_jwt_identity().get("user_id")
-    try:
-        n_pwd = r_body.get('new_password', None)
-        password = r_body.get('password', None)
-        email = r_body.get('email', None)
-        updated_user = user_service.update_user(user_id, password, n_pwd, email)
-        return {"message": updated_user}, 200
-    except InvalidParameter as e:
-        return {"message": str(e)}, 400
-    except Forbidden as e:
-        return {"message": str(e)}, 403
+    if request.method == "OPTIONS":
+        resp = flask.Response("preflight")
+        resp.headers["Access-Control-Allow-Origin"] = "http://127.0.0.1:5500"
+        resp.headers["Access-Control-Allow-Headers"] = "Content-Type, Content-Length, Access-Control-Allow-Credentials"
+        resp.headers["Access-Control-Allow-Methods"] = "PUT, OPTIONS"
+        resp.headers['Access-Control-Allow-Credentials'] = 'true'
+        return resp
+
+    elif request.method == "PUT":
+
+        r_body = request.get_json()
+        user_id = get_jwt_identity().get("user_id")
+        try:
+            n_pwd = r_body.get('new-password', None)
+            password = r_body.get('password', None)
+            email = r_body.get('email', None)
+            updated_user = user_service.update_user(user_id, password, n_pwd, email)
+            return {"message": updated_user}, 200
+        except InvalidParameter as e:
+            return {"message": str(e)}, 400
+        except Forbidden as e:
+            return {"message": str(e)}, 403
 
 
 @uc.route('/users', methods=['GET', 'OPTIONS'])
@@ -56,4 +66,5 @@ def get_user():
 
     elif request.method == "GET":
 
-        return {"user": get_jwt_identity().get('username')}, 200
+        return {"username": get_jwt_identity().get('username'),
+                "email": get_jwt_identity().get('email')}, 200
